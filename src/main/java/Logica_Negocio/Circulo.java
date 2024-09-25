1a49e8591e484a2c03f164d7789db4d3d04f0018
1059237155 <1059237155@SJ209PC6.fup.edu>
2024-09-25 17:01:20 -0500

    ejercicio

---
+++ src/main/java/Logica_Negocio/Circulo.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica_Negocio;
+
+/**
+ *
+ * @author 1059237155
+ */
+public class Circulo implements Figura{
+public double radio;
+
+    public Circulo() {
+    }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+
+
+    @Override
+    public double CalcularArea() {
+        return Math.PI*(Math.pow(radio, 2));
+    }
+
+    @Override
+    public void DibujarFigura() {
+        System.out.println(" ***");
+       System.out.println("* * * *");
+        System.out.println(" ***");
+        
+    }
+    
+}
---
+++ src/main/java/Logica_Negocio/Cuadrado.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica_Negocio;
+
+/**
+ *
+ * @author 1059237155
+ */
+public class Cuadrado implements Figura{
+    public double lado;
+
+    public Cuadrado() {
+    }
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+    
+
+    @Override
+    public double CalcularArea() {
+        return lado*lado;
+    }
+
+    @Override
+    public void DibujarFigura() {
+        System.out.println("****");
+        System.out.println("****");
+        System.out.println("****");
+    }
+    
+}
